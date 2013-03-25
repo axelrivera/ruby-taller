8a8b6ea297f604a03576da9fc0dcbc093301bc64
Axel Rivera <axel@riveralabs.com>
2013-03-24 23:23:12 -0400

    Add FizzBuzz

---
+++ fizz_buzz/fizz_buzz.rb
@@ -0,0 +1,9 @@
+# FizzBuzz Problem
+#
+# Write a program that prints the numbers from 1 to 100. 
+# But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”.  
+# For numbers which are multiples of both three and five print “FizzBuzz”.
+
+(1..100).to_a.each do |i|
+  # Put your code here
+end
